5e359472d91a1be9cf1e9db4cc8b00dc73845446
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-10-03 11:30:07 +0300

    Added Penguin and Hat classes

--- hatSwap/HatSwap.java
+++ hatSwap/HatSwap.java
@@ -1,5 +1,64 @@
 public class HatSwap {
-    public static void main(String[] args) {
-        System.out.println("Erase this line and code freely.");
+    public static final int NUM_PENGUIN = 5;
+    
+    public static void main(String[] args) throws InterruptedException{
+        Hat[] hats = new Hat[NUM_PENGUIN];
+        for (int i = 0; i < hats.length; i++) {
+            hats[i] = new Hat(i);
+        }
+        
+        Penguin[] penguins = new Penguin[NUM_PENGUIN];
+        for (int i = 0; i < penguins.length; i++) {
+            penguins[i] = new Penguin(i, hats[i]);
+        }
+        
+        for (Penguin peng : penguins) {
+            peng.start();
+        }
+        
+        for (Penguin peng : penguins) {
+            peng.join();
+        }
+    }
+}
+
+class Penguin extends Thread{
+    private Hat hat;
+    private int id;
+    public Penguin(int id, Hat hat) {
+        this.id = id;
+        this.hat = hat;
+    }
+    
+    public void run() {
+        System.out.println(this);
+    }
+    
+    public String toString() {
+        return "Penguin " + this.id;
+    }
+}
+
+class Hat {
+    private int id;
+    private boolean isTaken = false;
+    public Hat(int id) {
+        this.id = id;
+    }
+    
+    public void take() {
+        this.isTaken = true;
+    }
+    
+    public void put() {
+        this.isTaken = false;
+    }
+    
+    public boolean isTaken() {
+        return this.isTaken;
+    }
+    
+    public String toString() {
+        return "Hat " + this.id;
     }
 }
