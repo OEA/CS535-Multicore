0b3986d9488ab1116003c85cca7fe96650f232ee
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-10-03 23:24:08 +0300

    Finished assignment 1

--- hatSwap/HatSwap.java
+++ hatSwap/HatSwap.java
@@ -1,6 +1,6 @@
+
 public class HatSwap {
     public static final int NUM_PENGUIN = 5;
-    
     public static void main(String[] args) throws InterruptedException{
         Hat[] hats = new Hat[NUM_PENGUIN];
         for (int i = 0; i < hats.length; i++) {
@@ -12,6 +12,10 @@ public class HatSwap {
             penguins[i] = new Penguin(i, hats[i]);
         }
         
+        for (int i = 0; i < penguins.length; i++) {
+            penguins[i].setFrontPenguin((i != penguins.length -1) ? penguins[i+1] : penguins[0]);
+        }
+        
         for (Penguin peng : penguins) {
             peng.start();
         }
@@ -20,45 +24,86 @@ public class HatSwap {
             peng.join();
         }
     }
+    
 }
 
 class Penguin extends Thread{
     private Hat hat;
     private int id;
+    private Penguin frontPenguin;
     public Penguin(int id, Hat hat) {
         this.id = id;
         this.hat = hat;
     }
     
+    
+    public void setFrontPenguin(Penguin frontPenguin) {
+        this.frontPenguin = frontPenguin;
+    }
+    
     public void run() {
-        System.out.println(this);
+        while(true) {
+            try{
+                hat.take();
+                frontPenguin.hat.take();
+                
+                hat.put();
+                frontPenguin.hat.put();
+                
+                swap();
+                think();
+            } catch(InterruptedException e) {
+                return;
+            }
+        }
     }
     
     public String toString() {
         return "Penguin " + this.id;
     }
+    
+    private void think() throws InterruptedException {
+        sleep(500);
+        System.out.println(this + " is thinking...");
+    }
+    
+    private void swap() throws InterruptedException {
+        System.out.println(this + " is swapping with " + frontPenguin);
+        Hat temp = frontPenguin.hat;
+        frontPenguin.hat = this.hat;
+        this.hat = temp;
+    }
+    
 }
 
 class Hat {
     private int id;
-    private boolean isTaken = false;
+    private boolean isTaken;
     public Hat(int id) {
         this.id = id;
-    }
-    
-    public void take() {
-        this.isTaken = true;
-    }
-    
-    public void put() {
         this.isTaken = false;
     }
     
+    public synchronized void take() {
+        while (isTaken()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        isTaken = true;
+    }
+    
+    public synchronized void put() {
+        isTaken = false;
+        notifyAll();
+    }
+    
     public boolean isTaken() {
-        return this.isTaken;
+        return isTaken;
     }
     
     public String toString() {
-        return "Hat " + this.id;
+        return "Hat " + id;
     }
 }
